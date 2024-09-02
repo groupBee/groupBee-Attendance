78a8e2ffb08bf723524b7f3894f7f09c4191f6f5
Jinwook Jung <jeenuk.chung@gmail.com>
2024-09-02 17:14:46 +0900

    근태관리용 spring boot 라이브러리 (출근, 퇴근, 근태)

---
+++ src/main/java/groupbee/attendance/service/feign/FeignClient.java
@@ -0,0 +1,12 @@
+package groupbee.attendance.service.feign;
+
+import groupbee.attendance.config.FeignConfig;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@org.springframework.cloud.openfeign.FeignClient(name = "employee", url = "${FEIGN_BASE_URL}", configuration = FeignConfig.class)
+public interface FeignClient {
+    @GetMapping("/api/employee/info")
+    Map<String, Object> getEmployeeInfo();
+}
